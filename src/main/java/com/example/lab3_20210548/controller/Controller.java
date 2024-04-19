469d21de21bde3ce2693d00ddd8f2e5331b00e0f
May3012 <139161004+May3012@users.noreply.github.com>
2024-04-18 21:28:56 -0500

    Listado de clinicas completo

--- src/main/java/com/example/lab3_20210548/controller/Controller.java
+++ src/main/java/com/example/lab3_20210548/controller/Controller.java
@@ -1,7 +1,28 @@
 package com.example.lab3_20210548.controller;
 
+import com.example.lab3_20210548.repository.ClinicaRepository;
+import com.example.lab3_20210548.repository.OftalmologoRepository;
+import com.example.lab3_20210548.repository.PacienteRepository;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
 @org.springframework.stereotype.Controller
 public class Controller {
-    final
+    final ClinicaRepository clinicaRepository;
+
+
+    public Controller(ClinicaRepository clinicaRepository) {
+        this.clinicaRepository = clinicaRepository;
+
+    }
+
+    @GetMapping("/listarClinicas")
+    public String listar(Model model) {
+
+        model.addAttribute("listaClinicas", clinicaRepository.findAll());
+        return "listaClinicas";
+    }
+
+
 
 }
