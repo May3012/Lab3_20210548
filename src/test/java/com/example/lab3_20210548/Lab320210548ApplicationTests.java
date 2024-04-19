6365f132e480585253eb549d18e3c4f72443df25
May3012 <139161004+May3012@users.noreply.github.com>
2024-04-18 20:39:23 -0500

    Primer commit

---
+++ src/test/java/com/example/lab3_20210548/Lab320210548ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab3_20210548;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab320210548ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
